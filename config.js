7e3d8b7d4ce3f4db55c3852ba13f5ca9122878b3
Alessandro Maroso <alessandro.maroso@iks.it>
2016-04-01 09:21:29 +0200

    Added env variables to config.

--- config.js
+++ config.js
@@ -2,8 +2,9 @@ var config = {};
 
 
 config.TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
-config.TELEGRAM_CHAT_ID = -138481158;
-config.MUMBLE_URL = 'mumble://example.org';
-config.MUMBLE_USER = '';
+config.TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+config.MUMBLE_URL = process.env.MUMBLE_URL;
+config.MUMBLE_USER = process.env.MUMBLE_USER;
+config.MUMBLE_PASSWORD = process.env.MUMBLE_PASSWORD;
 
 module.exports = config;
